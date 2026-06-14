21870c59ce61f6abc5c8c440e121a29bff0b7263
“Ankan-dev” <ankanmandal2001@gmail.com>
2026-06-15 01:35:17 +0530

    applied the fixes in windows problem

--- src/index.ts
+++ src/index.ts
@@ -11,134 +11,142 @@ import { GenerateMongoDbSetup } from "./Database/mongodbSetup";
 import { GenerateDrizzleSetup } from "./Database/DrizzleSetup";
 import { EjsSetup } from "./Template_Engine/EjsSetup";
 
-const main = async ()=>{
+const main = async () => {
     const projectName = process.argv[2] || "my-app";
 
     const language = await inquirer.select({
         message: chalk.cyan("Select your language: "),
-        choices:[
+        choices: [
             {
-                name:chalk.yellow("Javascript"),
-                value:"Javascript"
+                name: chalk.yellow("Javascript"),
+                value: "Javascript"
             },
             {
-                name:chalk.blue("Typescript"),
-                value:"Typescript"
+                name: chalk.blue("Typescript"),
+                value: "Typescript"
             }
         ]
     })
 
     const packageManager = await inquirer.select({
-        message:chalk.cyan("Select Your Package manager"),
-        choices:[
+        message: chalk.cyan("Select Your Package manager"),
+        choices: [
             {
-                name:chalk.red("NPM"),
-                value:"npm"
+                name: chalk.red("NPM"),
+                value: "npm"
             },
             {
-                name:chalk.blue("YARN"),
-                value:"yarn"
+                name: chalk.blue("YARN"),
+                value: "yarn"
             },
             {
-                name:chalk.yellow("PNPM"),
-                value:"pnpm"
+                name: chalk.yellow("PNPM"),
+                value: "pnpm"
             }
         ]
     })
 
-    const templateDir = path.join(__dirname,'../template',language)
-    const projectDir = projectName==="."?process.cwd(): path.join(process.cwd(),projectName)
+    const templateDir = path.join(__dirname, '../template', language)
+    const projectDir = projectName === "." ? process.cwd() : path.join(process.cwd(), projectName)
 
-    fs.copySync(templateDir,projectDir)
+    fs.copySync(templateDir, projectDir, {
+        filter: (src) => !src.includes('node_modules')
+    })
 
-    switch(packageManager){
-        case "npm":{
-            execSync("npm install",{
-                cwd:projectDir,
-                stdio:"inherit"
+    const gitignoreSrc = path.join(projectDir, 'gitignore')
+    const gitignoreDest = path.join(projectDir, '.gitignore')
+    if (fs.existsSync(gitignoreSrc)) {
+        fs.renameSync(gitignoreSrc, gitignoreDest)
+    }
+
+    switch (packageManager) {
+        case "npm": {
+            execSync("npm install", {
+                cwd: projectDir,
+                stdio: "inherit"
             })
             break
         }
 
-        case "yarn":{
-            execSync("yarn install",{
-                cwd:projectDir,
-                stdio:"inherit"
+        case "yarn": {
+            execSync("yarn install", {
+                cwd: projectDir,
+                stdio: "inherit"
             })
             break
         }
 
-        case "pnpm":{
-            execSync("pnpm install",{
-                cwd:projectDir,
-                stdio:"inherit"
+        case "pnpm": {
+            execSync("pnpm install", {
+                cwd: projectDir,
+                stdio: "inherit"
             })
             break
         }
 
-        default:{
+        default: {
             console.log(chalk.red("Nothing is selected"))
         }
     }
 
     const needDb = await inquirer.select({
-        message:chalk.cyan("Do you want to setup a database in your project?"),
-        choices:[
+        message: chalk.cyan("Do you want to setup a database in your project?"),
+        choices: [
             {
-                name:chalk.green("Yes"),
-                value:true
+                name: chalk.green("Yes"),
+                value: true
             },
             {
-                name:chalk.red("No"),
-                value:false
+                name: chalk.red("No"),
+                value: false
             }
         ]
     })
     let dbChoice = ""
-    if(needDb){
+    if (needDb) {
         dbChoice = await inquirer.select({
-            message:chalk.cyan("Select your database: "),
-            choices:[
+            message: chalk.cyan("Select your database: "),
+            choices: [
                 {
-                    name:chalk.green("MongoDB"),
-                    value:"MongoDB"
+                    name: chalk.green("MongoDB"),
+                    value: "MongoDB"
                 },
                 // {
                 //     name:chalk.green("PostgreSQL"),
                 //     value:"PostgreSQL"
                 // },
                 {
-                    name:chalk.green("MySQL"),
-                    value:"MySQL"
+                    name: chalk.green("MySQL"),
+                    value: "MySQL"
                 }
             ]
         })
 
-        if(dbChoice === "MongoDB"){
-            GenerateMongoDbSetup(projectDir,packageManager,language)
+        if (dbChoice === "MongoDB") {
+            GenerateMongoDbSetup(projectDir, packageManager, language)
         } else {
             console.log("DB choice: ", dbChoice)
-            GenerateDrizzleSetup(projectDir,packageManager,language,dbChoice)
+            GenerateDrizzleSetup(projectDir, packageManager, language, dbChoice)
         }
     }
 
     const templateChoice = await inquirer.select({
-    message:chalk.cyan("Do you want to add EJS template engine in your project?"),
-    choices:[
-        {
-            name:chalk.green("Yes"),
-            value:true
-        },
-        {
-            name:chalk.red("No"),
-            value:false
-        }
-    ]
-   })
+        message: chalk.cyan("Do you want to add EJS template engine in your project?"),
+        choices: [
+            {
+                name: chalk.green("Yes"),
+                value: true
+            },
+            {
+                name: chalk.red("No"),
+                value: false
+            }
+        ]
+    })
 
-    if(templateChoice){
-        
-        EjsSetup(projectDir,packageManager,language,dbChoice)
+    if (templateChoice) {
+
+        EjsSetup(projectDir, packageManager, language, dbChoice)
     }
 
     console.log(chalk.green(`your express setup with ${language} is ready....`))
